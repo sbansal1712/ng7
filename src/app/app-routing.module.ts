e8ddba88eab4f32e20fc90879c419579a0fd09d8
sbansal1712 <Shubham1.Bansal@ril.com>
2019-06-07 01:02:51 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {path: '',component:HomeComponent},
+  {path: 'home',component:HomeComponent},
   {path:'about',component:AboutComponent},
   {path:'contact',component:ContactComponent}
 ];
