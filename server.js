068f00b56c526d247de2b58c610fe22b0c1e926f
sbansal1712 <Shubham1.Bansal@ril.com>
2019-06-07 13:45:14 +0530

    u

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-const path = require('path');
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Heroku automagically gives us SSL
-// Lets write some middleware to redirect us
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/{{ng7}}'));
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/ng7'));
-
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/ng7/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/{{ng7}}/index.html'));
 });
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);
-
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
 
 
