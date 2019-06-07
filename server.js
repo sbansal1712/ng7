b0cce781180ec31da686d8fcb96917bac0a8ec6c
sbansal1712 <Shubham1.Bansal@ril.com>
2019-06-07 15:26:06 +0530

    y

--- server.js
+++ server.js
@@ -16,16 +16,3 @@ app.get('/*', function(req, res) {
 
 // default Heroku port
 app.listen(process.env.PORT || 5000);
-
-let env = process.env.NODE_ENV || 'development';
-
-let forceSSL = (req, res, next) => {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  }
-  return next();
-};
-
-if (env === 'production') {
-  app.use(forceSSL);
-}
