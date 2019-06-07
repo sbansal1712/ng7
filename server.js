1f5cccb08d60264f96e6a3fa7f025b9545af8ae6
sbansal1712 <Shubham1.Bansal@ril.com>
2019-06-07 15:17:52 +0530

    u

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const app = express();
 
 // Heroku automagically gives us SSL
 // Lets write some middleware to redirect us
+
+// Serve static files
+app.use(express.static(__dirname + '/dist/ng7'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/ng7/index.html'));
+});
+
+// default Heroku port
+app.listen(process.env.PORT || 5000);
+
 let env = process.env.NODE_ENV || 'development';
 
 let forceSSL = (req, res, next) => {
@@ -17,13 +29,3 @@ let forceSSL = (req, res, next) => {
 if (env === 'production') {
   app.use(forceSSL);
 }
-// Serve static files
-app.use(express.static(__dirname + '/dist/ng7'));
-
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/ng7/index.html'));
-});
-
-// default Heroku port
-app.listen(process.env.PORT || 5000);
