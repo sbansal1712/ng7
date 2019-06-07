6a518ec8ad859ba1c2732bef59b86e3511268e1f
sbansal1712 <Shubham1.Bansal@ril.com>
2019-06-07 15:03:07 +0530

    updated

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-//Install express server
-const express = require('express');
-const path = require('path');
 
+const path = require('path');
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/ng7'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/ng7'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/ng7/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/ng7/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
-
-
+// default Heroku port
+app.listen(process.env.PORT || 5000);
